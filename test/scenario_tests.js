0b00ef69c96bb13ae7a955c16250caad3e9b8fee
RJ <surrealz@gmail.com>
2017-09-30 08:21:36 +0800

    reduced scenario 6 to one it() as directed by b9-xavier

--- test/scenario_tests.js
+++ test/scenario_tests.js
@@ -471,62 +471,6 @@ contract('TollBoothOperator', function (accounts) {
                     .then(tx => operator.reportExitRoad(secret0, { from: booth2 }))
             });
 
-            it("should be possible to set the base route price below both deposits and reduce count by 1", function () {
-                return operator.setRoutePrice.call(booth0, booth2, deposit0, { from: owner1 })
-                    .then(success => assert.isTrue(success))
-                    .then(() => operator.setRoutePrice(booth0, booth2, deposit0, { from: owner1 }))
-                    .then(tx => {
-                        assert.strictEqual(tx.receipt.logs.length, 2);
-                        assert.strictEqual(tx.logs.length, 2);
-                        const logPriceSet = tx.logs[0];
-                        assert.strictEqual(logPriceSet.event, "LogRoutePriceSet");
-                        assert.strictEqual(logPriceSet.args.sender, owner1);
-                        assert.strictEqual(logPriceSet.args.entryBooth, booth0);
-                        assert.strictEqual(logPriceSet.args.exitBooth, booth2);
-                        assert.strictEqual(logPriceSet.args.priceWeis.toNumber(), deposit0);
-                        const logExited = tx.logs[1];
-                        assert.strictEqual(logExited.event, "LogRoadExited");
-                        assert.strictEqual(logExited.args.exitBooth, booth2);
-                        assert.strictEqual(logExited.args.exitSecretHashed, hashed1);
-                        assert.strictEqual(logExited.args.finalFee.toNumber(), deposit0 * multiplier1);
-                        assert.strictEqual(
-                            logExited.args.refundWeis.toNumber(),
-                            (extraDeposit1 - deposit0) * multiplier1);
-                        // console.log(tx.receipt.gasUsed);
-                        return Promise.allNamed({
-                            hashed0: () => operator.getVehicleEntry(hashed0),
-                            hashed1: () => operator.getVehicleEntry(hashed1),
-                            pendingCount01: () => operator.getPendingPaymentCount(booth0, booth1),
-                            pendingCount02: () => operator.getPendingPaymentCount(booth0, booth2)
-                        });
-                    })
-                    .then(info => {
-                        assert.strictEqual(info.hashed0[0], vehicle0);
-                        assert.strictEqual(info.hashed0[1], booth0);
-                        assert.strictEqual(info.hashed0[2].toNumber(), extraDeposit0 * multiplier0);
-                        assert.strictEqual(info.hashed1[0], vehicle1);
-                        assert.strictEqual(info.hashed1[1], booth0);
-                        assert.strictEqual(info.hashed1[2].toNumber(), 0);
-                        assert.strictEqual(info.pendingCount01.toNumber(), 0);
-                        assert.strictEqual(info.pendingCount02.toNumber(), 1);
-                        return Promise.allNamed({
-                            operator: () => web3.eth.getBalancePromise(operator.address),
-                            collected: () => operator.getCollectedFeesAmount(),
-                            vehicle0: () => web3.eth.getBalancePromise(vehicle0),
-                            vehicle1: () => web3.eth.getBalancePromise(vehicle1)
-                        });
-                    })
-                    .then(balances => {
-                        assert.strictEqual(
-                            balances.operator.toNumber(),
-                            extraDeposit0 * multiplier0 + deposit0 * multiplier1);
-                        assert.strictEqual(balances.collected.toNumber(), deposit0 * multiplier1);
-                        assert.strictEqual(balances.vehicle0.toString(10), vehicle0InitBal.toString(10));
-                        assert.strictEqual(
-                            balances.vehicle1.toString(10),
-                            vehicle1InitBal.plus((extraDeposit1 - deposit0) * multiplier1).toString(10));
-                    });
-            });
 
             describe("Clear one more pending payment", function () {
 
